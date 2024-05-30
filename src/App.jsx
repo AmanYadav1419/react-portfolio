96b951cedcee891811ee01caf409dee2b79a1d16
unknown <amanyadav16544@gmail.com>
2024-05-30 21:38:15 +0530

    Completed Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
 
 const Trail = () => {
   return (
-    <h1 className='text-3xl font-bold underline'>Aman Yadav</h1>
-  )
-}
+    <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
+      <div className="flixed top-0 -z-10 h-full w-full"></div>
+      {/* black background is done using this div*/}
+      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
 
-export default Trail
+      <div className="container mx-auto px-8">
+        <Navbar />
+      </div>
+    </div>
+  );
+};
+
+export default Trail;
