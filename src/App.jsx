a8702e5d0d3d985401d940033b5f6355e37b06e7
unknown <amanyadav16544@gmail.com>
2024-05-31 10:28:04 +0530

    added About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
+import About from "./components/About";
 
 const Trail = () => {
   return (
     <div className="overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900">
-      <div className="flixed top-0 -z-10 h-full w-full"></div>
-      {/* black background is done using this div*/}
-      <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      <div className="fixed top-0 -z-10 h-full w-full">
+        {/* black background is done using this div*/}
+        <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
+      </div>
 
       <div className="container mx-auto px-8">
         <Navbar />
         <Hero />
+        <About />
       </div>
     </div>
   );
