ac5968e5fcc72b59323f067dfbe84a15dc57024e
unknown <amanyadav16544@gmail.com>
2024-06-02 11:07:21 +0530

    install framer-motion and animate Hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,16 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/kevinRushProfile.png";
+import { motion } from "framer-motion";
+
+// add a function to take delay parameter and a custom styling
+const container = (delay) => ({
+  hidden: { x: -100, opacity: 0 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    transition: { duration: 0.5, delay: delay },
+  },
+});
 
 const Hero = () => {
   return (
@@ -7,20 +18,41 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              variants={container(0)} //where 0 is the delay
+              initial="hidden"
+              animate="visible"
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
               Aman Yadav
-            </h1>
-            <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
+            </motion.h1>
+            <motion.span
+              variants={container(0.5)} //where 0.5 is the delay
+              initial="hidden"
+              animate="visible"
+              className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent"
+            >
               Full Stack Developer
-            </span>
-            <p className="my-2  max-w-xl py-6 font-light tracking-tighter">
+            </motion.span>
+            <motion.p
+              variants={container(1)} //where 1 is the delay
+              initial="hidden"
+              animate="visible"
+              className="my-2  max-w-xl py-6 font-light tracking-tighter"
+            >
               {HERO_CONTENT}
-            </p>
+            </motion.p>
           </div>
         </div>
         <div className="w-full lg:w-1/2 lg:p-8">
           <div className="flex justify-center">
-            <img src={profilePic} alt="Aman Yadav" />
+            <motion.img
+              initial={{ x: 100, opacity: 0 }}
+              animate={{ x: 0, opacity: 1 }}
+              transition={{ duration: 1, delay: 1.2 }}
+              src={profilePic}
+              alt="Aman Yadav"
+            />
           </div>
         </div>
       </div>
