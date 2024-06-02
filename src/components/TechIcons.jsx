9a1f12f913b114d7aa5c1313e034202f3cdfeee9
unknown <amanyadav16544@gmail.com>
2024-06-02 16:15:41 +0530

    animation done upto technologies section

--- src/components/TechIcons.jsx
+++ src/components/TechIcons.jsx
@@ -6,41 +6,90 @@ import { SiMongodb } from "react-icons/si";
 import { DiRedis } from "react-icons/di";
 import { FaNodeJs } from "react-icons/fa";
 import { BiLogoPostgresql } from "react-icons/bi";
+import { animate, motion } from "framer-motion";
+
+// function to implement animation
+const iconVariants = (duration) => ({
+  initial: { y: -10 },
+  animate: {
+    y: [10, -10],
+    transition: {
+      duration: duration,
+      ease: "linear",
+      repeat: Infinity,
+      repeatType: "reverse",
+    },
+  },
+});
 
 const TechIcons = () => {
   return (
-    <div className="flex flex-wrap items-center justify-center gap-4">
-      <div className="rounded-2xl border-4 border-neutral-800">
+    <motion.div
+      whileInView={{ opacity: 1, x: 0 }}
+      initial={{ opacity: 0, x: 100 }}
+      transition={{ duration: 1.3 }}
+      className="flex flex-wrap items-center justify-center gap-4"
+    >
+      <motion.div
+        variants={iconVariants(2.5)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800"
+      >
         {/* react icon */}
         <RiReactjsLine className="text-7xl text-cyan-400" />
-      </div>
+      </motion.div>
 
-      <div className="rounded-2xl border-4 border-neutral-800">
+      <motion.div
+        variants={iconVariants(4)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800"
+      >
         {/* Next icon */}
         <TbBrandNextjs className="text-7xl text-neutral-300" />
-      </div>
+      </motion.div>
 
-      <div className="rounded-2xl border-4 border-neutral-800">
+      <motion.div
+        variants={iconVariants(3.5)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800"
+      >
         {/* Mongodb icon */}
         <SiMongodb className="text-7xl text-green-400" />
-      </div>
+      </motion.div>
 
-      <div className="rounded-2xl border-4 border-neutral-800">
+      <motion.div
+        variants={iconVariants(2)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800"
+      >
         {/* Redis icon */}
         <DiRedis className="text-7xl text-red-500" />
-      </div>
+      </motion.div>
 
-      <div className="rounded-2xl border-4 border-neutral-800">
+      <motion.div
+        variants={iconVariants(4.5)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800"
+      >
         {/* NodeJs icon */}
         <FaNodeJs className="text-7xl text-green-500" />
-      </div>
+      </motion.div>
 
-      <div className="rounded-2xl border-4 border-neutral-800">
+      <motion.div
+        variants={iconVariants(3)}
+        initial="initial"
+        animate="animate"
+        className="rounded-2xl border-4 border-neutral-800"
+      >
         {/* Postgresql icon */}
         <BiLogoPostgresql className="text-7xl text-cyan-500" />
-      </div>
-      
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,9 +1,14 @@
 import TechIcons from "./TechIcons";
+import { motion } from "framer-motion";
 
 const Technologies = () => {
   return (
     <div className="border-b border-neutral-800 pb-24">
-        <h2 className="my-20 text-center text-4xl">Technologies</h2>
+        <motion.h2
+        whileInView={{opacity:1, y:0}} 
+        initial={{opacity:0, y:-100}}
+        transition={{duration:1.5}}
+        className="my-20 text-center text-4xl">Technologies</motion.h2>
         <div>
             {/* all icons */}
             <TechIcons />
