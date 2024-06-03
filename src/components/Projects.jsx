669fa93827a0639264028f11b51ecf84ccc130a6
unknown <amanyadav16544@gmail.com>
2024-06-03 16:17:15 +0530

    animation added to Projects Section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,14 +1,27 @@
 import { PROJECTS } from "../constants";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   return (
     <div className="border-b border-neutral-900 pb-4">
-      <h2 className="my-20 text-center text-4xl">Projects</h2>
+      <motion.h2
+        whileInView={{ opacity: 1, y: 0 }}
+        initial={{ opacity: 1, y: -100 }}
+        transition={{ duration: 0.5 }}
+        className="my-20 text-center text-4xl"
+      >
+        Projects
+      </motion.h2>
       <div>
         {PROJECTS.map((project, index) => {
           return (
             <div key={index} className="mb-8 flex flex-wrap lg:justify-center">
-              <div className="w-full lg:w-1/4">
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: -100 }}
+                transition={{ duration: 1 }}
+                className="w-full lg:w-1/4"
+              >
                 <img
                   src={project.image}
                   width={150}
@@ -16,8 +29,14 @@ const Projects = () => {
                   alt={project.title}
                   className="mb-6 rounded"
                 />
-              </div>
-              <div className="w-full max-w-xl lg:w-3/4">
+              </motion.div>
+
+              <motion.div
+                whileInView={{ opacity: 1, x: 0 }}
+                initial={{ opacity: 0, x: 100 }}
+                transition={{ duration: 1 }}
+                className="w-full max-w-xl lg:w-3/4"
+              >
                 <h6 className="mb-2 font-semibold">{project.title}</h6>
                 <p className="mb-4 text-neutral-400">{project.description}</p>
                 {project.technologies.map((tech, index) => {
@@ -30,7 +49,8 @@ const Projects = () => {
                     </span>
                   );
                 })}
-              </div>
+              </motion.div>
+
             </div>
           );
         })}
